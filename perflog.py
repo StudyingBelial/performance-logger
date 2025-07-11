5d5b18cef7d632b0ecf59450abfac222c23188c6
Belial <baarya24@tbc.edu.np>
2025-07-11 23:50:10 +0545

    Initial Commit with class structure

---
+++ perflog.py
@@ -0,0 +1,46 @@
+class Perflog():
+    def __init__(self):
+        pass
+
+    def __steup_logger(self):
+        pass
+
+    def log(self):
+        pass
+
+    def __get_lap(self):
+        pass
+
+    def __check_nvidia(self):
+        pass
+
+    def get_cpu_util(self):
+        pass
+
+    def get_cpu_clock(self):
+        pass
+
+    def get_process_cpu_util(self):
+        pass
+
+    def get_ram_usage(self):
+        pass
+
+    def get_process_ram_usage(self):
+        pass
+
+    def get_gpu_clock(self):
+        pass
+
+    def get_gpu_util(self):
+        pass
+
+    def get_vram_usage(self):
+        pass
+
+    def get_process_vram_usage(self):
+        pass
+
+    @property
+    def isNvidia(self):
+        pass
