0d296336e49bb6ad6f8e3c715b9b3652d2e33509
Belial <baarya24@tbc.edu.np>
2025-07-12 12:15:59 +0545

    Added logger setup

--- perflog.py
+++ perflog.py
@@ -1,11 +1,25 @@
+from imports import *
+
 class Perflog():
     def __init__(self):
-        pass
+        self.__steup_logger()
 
     def __steup_logger(self):
-        pass
+        self.__logger = logging.getLogger("PerfLog")
+        self.__logger.setLevel(logging.DEBUG)
+        cwd = os.path.abspath(os.getcwd())
+        log_dir_path = cwd + "/log"
+        log_file_path = log_dir_path + "/log.txt"
 
-    def log(self):
+        if not os.path.isdir(log_dir_path):
+            os.mkdir(log_dir_path)
+
+        handler = logging.FileHandler(log_file_path)
+        formatter = jsonlogger.JsonFormatter('%(asctime)s - %(message)s')
+        handler.setFormatter(formatter)
+        self.__logger.addHandler(handler)
+
+    def __log(self):
         pass
 
     def __get_lap(self):
@@ -14,31 +28,31 @@ class Perflog():
     def __check_nvidia(self):
         pass
 
-    def get_cpu_util(self):
+    def __get_cpu_util(self):
         pass
 
-    def get_cpu_clock(self):
+    def __get_cpu_clock(self):
         pass
 
-    def get_process_cpu_util(self):
+    def __get_process_cpu_util(self):
         pass
 
-    def get_ram_usage(self):
+    def __get_ram_usage(self):
         pass
 
-    def get_process_ram_usage(self):
+    def __get_process_ram_usage(self):
         pass
 
-    def get_gpu_clock(self):
+    def __get_gpu_clock(self):
         pass
 
-    def get_gpu_util(self):
+    def __get_gpu_util(self):
         pass
 
-    def get_vram_usage(self):
+    def __get_vram_usage(self):
         pass
 
-    def get_process_vram_usage(self):
+    def __get_process_vram_usage(self):
         pass
 
     @property
