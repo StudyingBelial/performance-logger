d282dd9b72165c4861c28c7f3217dc2b04d99ccc
Belial <baarya24@tbc.edu.np>
2025-07-12 12:33:38 +0545

    Initialized CPU Utilization and corrected log func calls

--- perflog.py
+++ perflog.py
@@ -3,6 +3,8 @@ from imports import *
 class Perflog():
     def __init__(self):
         self.__steup_logger()
+        self.__get_cpu_util()
+        self.__get_process_cpu_util()
 
     def __steup_logger(self):
         self.__logger = logging.getLogger("PerfLog")
@@ -25,10 +27,10 @@ class Perflog():
                 extra={
                     'runtime' : self.__total_runtime,
                     'lap' : self.__get_lap(),
-                    'cpu_utilization' : self.get_cpu_util(),
-                    'cpu_clockspeed' : self.get_cpu_clock(),
-                    'ram' : self.get_ram_usage(),
-                    'process ram' : self.get_process_ram_usage(),
+                    'cpu_utilization' : self.__get_cpu_util(),
+                    'cpu_clockspeed' : self.__get_cpu_clock(),
+                    'ram' : self.__get_ram_usage(),
+                    'process ram' : self.__get_process_ram_usage(),
                     'gpu_utilization' : self.__get_gpu_util(),
                     'gpu_clockspeed' : self.__get_gpu_clock(),
                     'vram' : self.__get_vram_usage(),
@@ -39,11 +41,11 @@ class Perflog():
                 extra={
                     'runtime' : self.__total_runtime,
                     'lap' : self.__get_lap(),
-                    'cpu_utilization' : self.get_cpu_util(),
-                    'cpu_clockspeed' : self.get_cpu_clock(),
-                    'ram' : self.get_ram_usage(),
-                    'process_cpu_utilization' : self.get_process_cpu_util(),
-                    'process_ram' : self.get_process_ram_usage(),
+                    'cpu_utilization' : self.__get_cpu_util(),
+                    'cpu_clockspeed' : self.__get_cpu_clock(),
+                    'ram' : self.__get_ram_usage(),
+                    'process_cpu_utilization' : self.__get_process_cpu_util(),
+                    'process_ram' : self.__get_process_ram_usage(),
                     'gpu_utilization' : "None",
                     'gpu_clockspeed' : "None",
                     'vram' : "None",
