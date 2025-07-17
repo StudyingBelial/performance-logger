03763e138aae5bf8c50ef9bd27b747bc812950dc
Belial <baarya24@tbc.edu.np>
2025-07-17 15:58:15 +0545

    Corrected erors caused by changes in the previous commit and remove userdefined logpath

--- perflog.py
+++ perflog.py
@@ -6,13 +6,12 @@ from pythonjsonlogger import jsonlogger
 from pynvml import *
 
 try:
-    import pynvml
     _NVML_AVAILABLE = True
 except ImportError:
     _NVML_AVAILABLE = False
 
 class Perflog:
-    def __init__(self, log_file_path = None):
+    def __init__(self):
         """
         Args:
             log_file_path: Incase the user does not want the CWD to create a folder called 'logs' and start writing to a file in that folder called 'log.txt'
@@ -25,10 +24,10 @@ class Perflog:
 
         """
         # Setup the file and logging variable
-        self.__steup_logger(log_file_path)
+        self.__steup_logger__()
 
         # Checking for a NVIDIA GPU
-        self.__isnvidia = self.__check_nvidia()
+        self.__isnvidia = self.__check_nvidia__()
 
         # Setting up universal NVML handel if a NVIDIA GPU is detected
         if self.__isnvidia:
@@ -40,17 +39,12 @@ class Perflog:
         self.__last_lap_time = 0
 
         # First initialization
-        self.__get_lap()
+        self.get_lap()
 
         # Creating an object for the current process
         self.__current_process = psutil.Process(os.getpid())
 
-        # Initilization step to start counting as these two functions have to be initialized once before a log is created
-        # since this initialization creates the base for the system to start counting
-        self.__get_cpu_util()
-        self.__get_process_cpu_util()
-
-    def __steup_logger(self, log_file_path):
+    def __steup_logger__(self):
         """
         Args:
             log_file_path: Incase the user does not want the CWD to create a folder called 'logs' and start writing to a file in that folder called 'log.txt'
@@ -60,12 +54,10 @@ class Perflog:
         self.__logger = logging.getLogger("PerfLog")
         self.__logger.setLevel(logging.DEBUG)
         cwd = os.path.abspath(os.getcwd())
-        if not log_file_path:
-            log_dir_path = cwd + "/logs"
-            log_file_path = log_dir_path + "/log.txt"
-
+        log_dir_path = cwd + "/logs"
+        log_file_path = log_dir_path + "/perflog.txt"
         if not os.path.isdir(log_dir_path):
-            os.mkdir(log_dir_path)
+                os.mkdir(log_dir_path)
 
         handler = logging.FileHandler(log_file_path)
         formatter = jsonlogger.JsonFormatter('%(asctime)s - %(levelname)s - %(message)s')
@@ -77,29 +69,29 @@ class Perflog:
             self.__logger.debug(log_message,
                 extra={
                     'runtime' : self.__total_runtime,
-                    'lap' : self.__get_lap(),
-                    'cpu_utilization' : self.__get_cpu_util(),
-                    'cpu_clockspeed' : self.__get_cpu_clock(),
-                    'ram' : self.__get_ram_usage(),
-                    'process ram' : self.__get_process_ram_usage(),
-                    'gpu_utilization' : self.__get_gpu_util(),
-                    'gpu_clockspeed' : self.__get_gpu_clock(),
-                    'vram' : self.__get_vram_usage(),
-                    'process_vram' : self.__get_process_vram_usage()
+                    'lap' : self.get_lap(),
+                    'cpu_utilization' : self.get_cpu_util(),
+                    'cpu_clockspeed' : self.get_cpu_clock(),
+                    'ram' : self.get_ram_usage(),
+                    'process ram' : self.get_process_ram_usage(),
+                    'gpu_utilization' : self.get_gpu_util(),
+                    'gpu_clockspeed' : self.get_gpu_clock(),
+                    'vram' : self.get_vram_usage(),
+                    'process_vram' : self.get_process_vram_usage()
                     })
         else:
             self.__logger.debug(log_message,
                 extra={
                     'runtime' : self.__total_runtime,
-                    'lap' : self.__get_lap(),
-                    'cpu_utilization' : self.__get_cpu_util(),
-                    'cpu_clockspeed' : self.__get_cpu_clock(),
-                    'ram' : self.__get_ram_usage(),
-                    'process_cpu_utilization' : self.__get_process_cpu_util(),
-                    'process_ram' : self.__get_process_ram_usage()
+                    'lap' : self.get_lap(),
+                    'cpu_utilization' : self.get_cpu_util(),
+                    'cpu_clockspeed' : self.get_cpu_clock(),
+                    'ram' : self.get_ram_usage(),
+                    'process_cpu_utilization' : self.get_process_cpu_util(),
+                    'process_ram' : self.get_process_ram_usage()
                     })
 
-    def __get_lap(self):
+    def get_lap(self):
         """
         Returns the time elapsed since the last call to this method (in seconds).
         If it's the first call after initialization, it returns time since instance creation.
@@ -117,19 +109,19 @@ class Perflog:
             self.__logger.error(f"Error getting lap time: {e}", exc_info=True)
             return None
 
-    def __get_cpu_util(self):
+    def get_cpu_util(self):
         """
         Returns the current system-wide CPU utilization as a percentage.
         This function does not have an interval to interrupt process
         """
         try:
             # interval is the amount of seconds the system will pause to calculate the CPU usage for efficient execution it is None
-            return psutil.cpu_percent(interval = None)
+            return psutil.cpu_percent(interval = 0.01)
         except Exception as e:
             self.__logger.error(f"Error getting the CPU Utilization. {e}")
-            return None
+            return 0.0
 
-    def __get_cpu_clock(self):
+    def get_cpu_clock(self):
         """
         Returns the current CPU Clock Speed in Mhz(Mega Hertz).
         """
@@ -139,7 +131,7 @@ class Perflog:
             self.__logger.error(f"Error getting the CPU Clock Speed. {e}")
             return None
 
-    def __get_process_cpu_util(self):
+    def get_process_cpu_util(self):
         """
         Returns the CPU Utilization of the current Python process as a percentage.
         Requires calling this function twice with an interval for an accurate reading.
@@ -150,11 +142,12 @@ class Perflog:
             # psutil.cpu_percent() for a process calculates the percentage
             # since the last call to this method, or since process start if first call.
             # For a meaningful "snapshot" after some work, call it once before and once after.
-            return self.__current_process.cpu_percent(interval= None)
+            return self.__current_process.cpu_percent(interval= 0.01)
         except Exception as e:
-            self.__logger.error("Error getting the process CPU Utilization. {e}")
+            self.__logger.error(f"Error getting the process CPU Utilization. {e}")
+            return 0.0
 
-    def __get_ram_usage(self):
+    def get_ram_usage(self):
         """
         Returns the system-wide physical RAM in MB(Megabytes).
         """
@@ -164,7 +157,7 @@ class Perflog:
             self.__logger.error(f"Error getting RAM usage. {e}")
             return None
 
-    def __get_process_ram_usage(self):
+    def get_process_ram_usage(self):
         """
         Returns the Resident Set Size (RSS) memory usage of the current Python process in MB(MegaBytes).
         RSS is the portion of memory held in RAM.
@@ -175,11 +168,12 @@ class Perflog:
             self.__logger.error(f"Error getting Process RAM usage (RSS). {e}")
             return None
 
-    def __check_nvidia(self):
+    def __check_nvidia__(self):
         try:
             if not _NVML_AVAILABLE:
                 return False
             else:
+                nvmlInit()
                 device_count = nvmlDeviceGetCount()
                 if device_count > 0:
                     return True
@@ -189,12 +183,12 @@ class Perflog:
                 self.__logger.error(f"Eror checking for NVIDIA graphics card. So no card exists. {e}")
                 return False
 
-    def __get_gpu_clock(self):
+    def get_gpu_clock(self):
         """
         Returns the current graphics clock speed in MHz for the first detected NVIDIA GPU.
         Returns None if pynvml is not available or no NVIDIA GPU is found.
         """
-        if self.__check_nvidia:
+        if self.__isnvidia:
             try:
                 # Get graphics clock info (NVML_CLOCK_GRAPHICS) at current level (NVML_CLOCK_ID_CURRENT)
                 clock_speed = nvmlDeviceGetClockInfo(self.__handle, NVML_CLOCK_GRAPHICS)
@@ -203,7 +197,7 @@ class Perflog:
                 self.__logger.error(f"Unexpected error getting GPU clock speed: {e}")
                 return None
 
-    def __get_gpu_util(self):
+    def get_gpu_util(self):
         """
         Returns the GPU utilization as a percentage for the first detected NVIDIA GPU.
         Returns None if pynvml is not available or no NVIDIA GPU is found.
@@ -216,7 +210,7 @@ class Perflog:
                 self.__logger.error(f"Unexpected error getting GPU utilization: {e}")
                 return None
 
-    def __get_vram_usage(self):
+    def get_vram_usage(self):
         """
         Returns the used VRAM in Megabytes (MB) for the first detected NVIDIA GPU.
         Returns None if pynvml is not available or no NVIDIA GPU is found.
@@ -230,7 +224,7 @@ class Perflog:
                 self.__logger.error(f"Unexpected error getting VRAM usage: {e}")
                 return None
 
-    def __get_process_vram_usage(self):
+    def get_process_vram_usage(self):
         """
         Attempts to return the VRAM usage of the current Python process in Megabytes (MB)
         for NVIDIA GPUs. This relies on pynvml's process listing.
