98a51482d932c594ef52004bc764d6e3f022e257
Belial <baarya24@tbc.edu.np>
2025-07-20 15:20:15 +0545

    Adding README.md features to the PyPi project and updating the install_requires to match requirements.txt

--- setup.py
+++ setup.py
@@ -1,8 +1,18 @@
 from setuptools import setup, find_packages
 
+with open("README.md", "r") as f:
+    description = f.read()
+
 setup(
     name='sb_perflog',
-    vesion='1.0',
+    vesion='0.1',
     packages=find_packages(),
-    install_requires = ['pynvml==12.0.0'],
+    install_requires = [
+        'pynvml==12.0.0',
+        'psutil==7.0.0',
+        'logging==0.4.9.6',
+        'pythonjsonlogger==3.3.0'
+        ],
+    long_description= description,
+    long_description_content_type="text/markdown",
 )
