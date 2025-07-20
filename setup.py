89a92ed0d3501994601ca7aeae25a97c2c56e760
Belial <baarya24@tbc.edu.np>
2025-07-20 15:37:30 +0545

    Updated the version

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ with open("README.md", "r") as f:
 
 setup(
     name='sb_perflog',
-    vesion='0.1',
+    version='0.1.1',
     packages=find_packages(),
     install_requires = [
         'pynvml==12.0.0',
