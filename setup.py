caf9434b87ae9690cf4c960b74f6bea2e73cd704
Belial <baarya24@tbc.edu.np>
2025-07-20 17:23:47 +0545

    Correcting errors with setup

--- setup.py
+++ setup.py
@@ -5,13 +5,12 @@ with open("README.md", "r") as f:
 
 setup(
     name='sb_perflog',
-    version='0.1.1',
+    version='0.1.2',
     packages=find_packages(),
     install_requires = [
         'pynvml==12.0.0',
         'psutil==7.0.0',
-        'logging==0.4.9.6',
-        'pythonjsonlogger==3.3.0'
+        'python-json-logger==3.3.0'
         ],
     long_description= description,
     long_description_content_type="text/markdown",
